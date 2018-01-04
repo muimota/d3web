651823600f43b31578f6c4b2c959e08787e7241d
Martin Nadal <martin.nadal@gmail.com>
2018-01-04 12:35:53 +0100

    people represented with circles

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 'use strict'
+//(c) 2018 Martin Nadal martin@muimota.net
 
 var svg = d3.select("#svgview"),
     margin = {top: 20, right: 40, bottom: 40, left: 80},
@@ -8,8 +9,12 @@ var svg = d3.select("#svgview"),
 var g = svg.append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var x = d3.scaleLinear().range([30,width])
+var yearX   = d3.scaleLinear().range([30,width])
+var peopleX = d3.scaleLinear().range([30,width])
 
+var tooltip = g.append("text")
+    .attr("class", "tooltip")
+    .style("opacity", 0)
 
 d3.json("projects_web.json",update)
 
@@ -18,16 +23,31 @@ function update(data){
 
   //remove work with no startYear
   data = data.filter(d => d.startYear != null && d.endYear != null)
-  data.forEach((p,i) => { if ( p.startYear == p.endYear ){p.endYear ++}})
+  let everybody = []
+  //project with same startDate endDate add a year so it has 1 year duration
+  data.forEach((p,i) => {
+    if ( p.startYear == p.endYear ){
+      p.endYear ++
+    }
+
+    let people = []
+    for (let department in p.team){
+      people = people.concat(p.team[department])
+    }
+    p['people'] = Array.from(new Set(people))
+    everybody = Array.from(new Set(everybody.concat(p['people'])))
+  })
+  everybody.sort()
+  //console.log(everybody);
   data.sort((a,b) => a.startYear - b.startYear)
   let domainExtent = [d3.min(data,d=>d.startYear),d3.max(data,d=>d.endYear)]
   //define el domino de x
-  x.domain(domainExtent);
+  yearX.domain(domainExtent);
 
   //dibuja la linea inferior
   g.append("g")
      .attr("class", "axis axis--x")
-     .call(d3.axisTop(x).tickFormat(d3.format('04')).ticks());
+     .call(d3.axisTop(yearX).tickFormat(d3.format('04')).ticks());
 
 
   let rowHeight = 10
@@ -38,7 +58,7 @@ function update(data){
     .enter()
       .append('rect')
       .attr('id',d => d.id )
-      .attr('x' ,d => x(d.startYear) + 1)
+      .attr('x' ,d => yearX(d.startYear) + 1)
       .attr('y' ,
         function(d){
           let i = 0
@@ -52,8 +72,41 @@ function update(data){
           rows[i] = d.endYear
           return i * rowHeight + 5
         })
-      .attr('width', d => x(d.endYear) -  x(d.startYear) - 1 )
+      .attr('width', d => yearX(d.endYear) -  yearX(d.startYear) - 1 )
       .attr('height', rowHeight - 1)
+
+    //tooltip
+    blocks.on('mouseover', function(d){
+
+      let node = d3.select(this)
+      node.attr('opacity',0.5)
+      //console.log(node.attr('x')+ node.attr('width') | 0)
+      tooltip.transition()
+        .duration(200)
+        .style("opacity", .9)
+
+      tooltip.html(d.shortname)
+        .attr('x',parseFloat(node.attr('x')) + parseFloat(node.attr('width')) / 2)
+        .attr('y',100)
+    })
+    blocks.on('mouseout', function(d){
+      let node = d3.select(this)
+      node.attr('opacity',1)
+      tooltip.transition()
+        .duration(200)
+        .style("opacity", 0)
+
+    })
+    peopleX.domain([0,everybody.length])
+
+    let people = g.selectAll('circle')
+      .data(everybody)
+      .enter()
+        .append('circle')
+        .attr('id',d => d.id )
+        .attr('r', d => 3)
+        .attr('cx',(d,i) => peopleX(i))
+        .attr('cy',(d,i) => 150 + (i % 3) * 6 )
 
 
 }
