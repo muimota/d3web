7004d74a4ef9b30b2bba2c01d3ed8564f679ceb7
Martin Nadal <martin.nadal@gmail.com>
2018-03-06 16:02:28 +0100

    DataModel to abstract the function of filtering and grabbing tags.

---
+++ src/DataModel.js
@@ -0,0 +1,57 @@
+class DataModel{
+
+  constructor(_data){
+    this.data = _data
+    this.tagKeys = ['space','atmosphere','materiality']
+  }
+
+  get tags(){
+
+    let tagsDict = {}
+    let projects = Object.values(this.data.projects)
+
+    for( let tagKey of this.tagKeys){
+      let tagArray = []
+      projects.filter( p => p.hasOwnProperty(tagKey)).map(p => tagArray = tagArray.concat(p[tagKey]))
+      tagsDict[tagKey] =  Array.from(new Set(tagArray)).sort()
+    }
+
+    return tagsDict
+
+  }
+  //select projects based on the
+  filter(tagsDict){
+
+    let projects = Object.values(this.data.projects)
+
+    let selectedProjects = projects.filter( p => {
+
+      for(let tagKey in tagsDict){
+
+        if( !p.hasOwnProperty(tagKey)){
+          continue
+        }
+
+        let tags = tagsDict[tagKey]
+
+        for(let tag of tags){
+          if(p[tagKey].includes(tag)){
+            return true
+          }
+        }
+        return false
+      }
+    })
+
+    let data = Object({},this.data)
+    data.projects = selectedProjects
+
+    return new DataModel(data)
+  }
+  //gets Tags that relate in some way
+  queryTags(projects){
+
+  }
+}
+
+export {DataModel}
